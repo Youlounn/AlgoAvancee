4def489af531d453d48a1c16cfaaad8215b52857
PonyHacker <Soudzy@gmail.com>
2016-11-07 17:11:24 +0100

    DelQRCode

--- test.php
+++ test.php
@@ -1,5 +1,6 @@
 <?php
 $numero = $_POST['1'];
 $ret = file_get_contents("https://www.cinemasgaumontpathe.com/index.php?do=reservation_white/ajax/checkNumeroCarte&v=".$numero."&sectionId=136&progId=27217976&nofid=1");
-echo $ret;
+$pd=json_decode($ret);
+var_dump ($pd);
  ?>
