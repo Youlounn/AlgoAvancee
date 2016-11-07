5bf74864762ee88245f399b23d313dfbc785ae52
Youlounn <yohann.rialet28@gmail.com>
2016-11-07 16:59:54 +0100

    Check seulement valid

--- test.php
+++ test.php
@@ -1,5 +1,7 @@
 <?php
 $numero = $_POST['1'];
-$ret = file_get_contents("https://www.cinemasgaumontpathe.com/index.php?do=reservation_white/ajax/checkNumeroCarte&v=".$numero."&sectionId=136&progId=27217976&nofid=1");
-echo $ret;
+(string) $ret = file_get_contents("https://www.cinemasgaumontpathe.com/index.php?do=reservation_white/ajax/checkNumeroCarte&v=".$numero."&sectionId=136&progId=27217976&nofid=1");
+$ret1 = utf8_encode($ret);
+$ret = json_decode($ret1);
+print_r ($ret->{'s'});
  ?>
