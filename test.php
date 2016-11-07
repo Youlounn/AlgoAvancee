2a1134441abcc49a0472fd25ccf79e1e78334860
Youlounn <yohann.rialet28@gmail.com>
2016-11-07 17:32:11 +0100

    Mise en forme réponse verif

--- test.php
+++ test.php
@@ -3,5 +3,12 @@ $numero = $_POST['1'];
 (string) $ret = file_get_contents("https://www.cinemasgaumontpathe.com/index.php?do=reservation_white/ajax/checkNumeroCarte&v=".$numero."&sectionId=136&progId=27217976&nofid=1");
 $ret1 = utf8_encode($ret);
 $ret = json_decode($ret1);
-print_r ($ret->{'s'});
+if($ret->{'s'} == 1){
+  $bool = 'valide';
+}
+else{
+  $bool = 'pas valide';
+}
+print_r ("Le numéro ".$ret->{'num'}." est ".$bool);
+
  ?>
